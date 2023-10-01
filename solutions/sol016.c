568029c5a5043915d8a72c5315511d6d5b008b36
aayushdhakal69 <therespawner69@gmail.com>
2023-10-01 11:30:43 +0545

    solution of problem 16

--- solutions/sol016.c
+++ solutions/sol016.c
@@ -8,7 +8,6 @@ int main() {
     printf("Enter the range: ");
     scanf("%d", &range);
     
-    // Print the multiplication table
     printf("Multiplication table of %d up to %d:\n", num, range);
     
     for (int i = 1; i <= range; i++) {
